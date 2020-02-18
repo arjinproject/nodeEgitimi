d83741dddeb1b5d91054defa44a61962b957c023
Hamit SEYREK <hamitseyrek@gmail.com>
2020-02-18 13:31:42 +0300

    diziler

--- temel-js/diziler.js
+++ temel-js/diziler.js
@@ -2,5 +2,14 @@ var dizi=["Merhmet", 28];
 
 console.log(dizi);  // tüm elemanlar yazdırılıyor
 
+console.log(dizi[0]);  // o. indexteki elemanı yazdırır
 
+for (var i = 0; i<dizi.length; i++){
+    console.log(dizi[i]);
+}
 
+console.log("For each kısmı başlıyor");
+
+dizi.forEach(function (k)  {
+    console.log(k);
+});

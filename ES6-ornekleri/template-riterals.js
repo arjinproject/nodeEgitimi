b16e7b1637e1e465c1356d739ad3467fdaaef271
Hamit SEYREK <hamitseyrek@gmail.com>
2020-02-18 16:28:23 +0300

    template-riterals

--- ES6-ornekleri/template-riterals.js
+++ ES6-ornekleri/template-riterals.js
@@ -4,4 +4,5 @@
 const name = "Hamit";
 const surname = "SEYREK";
 
-console.log(`Benim adım ${name} soyadım ${surname}`); // ters tırnak işareti için mac te option+; kullanıyoruz
+console.log(`Benim adım ${name} soyadım ${surname}`); 
+// ters tırnak işareti için mac te option+; kullanıyoruz
